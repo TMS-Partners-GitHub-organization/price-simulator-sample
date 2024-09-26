21ec95fd9afbd4742a6230b5c0924037df81337c
JunyaKido <j.kido@tms-partners.com>
2024-09-26 20:56:08 +0900

    数値変更

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,120 @@
 import './style.css'
 import Alpine from 'alpinejs'
 
+// const basePrice = {
+//   simple: {
+//     "hokan": 66000,
+//     "shoki": 22000,
+//     "kensa": 33000,
+//     "sagyo": 0,
+//     "gijutsu": 236600,
+//   },
+//   standard: {
+//     "hokan": 132000,
+//     "shoki": 44000,
+//     "kensa": 33000,
+//     "sagyo": 32000,
+//     "gijutsu": 236600,
+//   }
+// }
+
 Alpine.data('simulator', () => ({
-  base: 357600,
-  dochiramo: false,
-  choki: 'none' as 'none' | 'single' | 'double',
-  ikkatsu: false,
-  repeater: false,
-  get dividedBy120() {
-    return Math.round(this.total / 120)
-  },
-  get total() {
-    let total = this.base
-
-    // さい帯・さい帯血 どちらも保管【早割】
-    if (this.dochiramo) total += 20000
-
-    // 長期保管オプション（20年保管）
-    const chokiObj = {
-      none: 0,
-      single: 25000,
-      double: 50000
+  plan: "simple",
+  hokan: "10",
+  kaisu: "ikkatsu",
+  discount: [] as string[],
+  get hokanPrice() {
+    let price = 0;
+    // プラン選択
+    if (this.plan === "simple") {
+      price = 66000;
     }
-    total += chokiObj[this.choki]
+    if (this.plan === "standard") {
+      price = 132000;
+    }
+    // 年数
+    if (this.hokan === "20") {
+      price *= 2
+    }
+    return price;
+  },
+  get shokiPrice() {  
+    let price = 0;
+    if (this.plan === "simple") {
+      price = 22000;
+    }
+    if (this.plan === "standard") {
+      price = 44000;
+    }
+    return price;
+  },
+  kensaPrice: 33000,
+  gijutsuPrice: 236600,
+  get sagyoPrice() {
+    let price = 0;
+    if (this.plan === "standard") {
+      price = 32000;
+    }
+    return price;
+  },
+  get totalPrice() {
+    let price = this.hokanPrice + this.shokiPrice + this.kensaPrice + this.gijutsuPrice + this.sagyoPrice;
+    // リピーター割引
+    if(this.discount.includes("repeater")){
+      price -= 30000;
+    }
+    //　多胎割引
+    if(this.discount.includes("tatai")){
+      price -= 100000;
+    }
+    // 一括割引 
+    if(this.kaisu ==="ikkatsu"){
+      price -= 15000;
+    }
+    return price;
+  },
+  get monthlyPrice(){
+    let price = this.totalPrice;
+    // 一括
+    if(this.kaisu === "ikkatsu"){
+      return null;
+    }
+    // 回数で割る
+    price /= Number(this.kaisu);
+    // 小数点以下切り捨て
+    price = Math.floor(price);
+    return price ;
+  },
+  // base: 357600,
+  // dochiramo: false,
+  // choki: 'none' as 'none' | 'single' | 'double',
+  // ikkatsu: false,
+  // repeater: false,
+  // get dividedBy120() {
+  //   return Math.round(this.total / 120)
+  // },
+  // get total() {
+  //   let total = this.base
 
-    // 一括払い割引
-    if (this.ikkatsu) total -= 15000
+  //   // さい帯・さい帯血 どちらも保管【早割】
+  //   if (this.dochiramo) total += 20000
 
-    // リピーター様割引
-    if (this.repeater) total -= 20000
+  //   // 長期保管オプション（20年保管）
+  //   const chokiObj = {
+  //     none: 0,
+  //     single: 25000,
+  //     double: 50000
+  //   }
+  //   total += chokiObj[this.choki]
 
-    return total
-  }
+  //   // 一括払い割引
+  //   if (this.ikkatsu) total -= 15000
+
+  //   // リピーター様割引
+  //   if (this.repeater) total -= 20000
+
+  //   return total
+  // }
 }))
 
 // @ts-ignore
